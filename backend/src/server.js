dcc9df97d14603e325b1a1a01b874556f8d9e1db
Tripchanski <arturtripking01@gmail.com>
2025-09-21 10:14:40 +0300

    Add User Routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
-
+import cors from 'cors';
+import {clerkMiddleware} from "@clerk/express";
+import userRoutes from './routes/user.route.js';
 
 const app = express();
 
+app.use(cors())
+app.use(express.json());
+app.use(clerkMiddleware());
+
 app.get('/', (req, res) => res.send('API is running...'));
+
+app.use("api/users", userRoutes);
 
 const startServer = async () => {
     try {
